1beb9bf3daadeb4e8fcdc7cb4ad6a26fc5b00b3e
sonalparab <sparab@stuy.edu>
2018-02-09 21:25:54 -0500

    Rectangle

--- canvas.js
+++ canvas.js
@@ -3,7 +3,10 @@ var ctx = canvas.getContext('2d');
 var stop = document.getElementById("stop");
 var start = document.getElementById("start");
 var animate = document.getElementById("animate");
+var bounce = document.getElementById("bounce");
+var cont = document.getElementById("continue");
 
+//Circle
 var requestID;
 var x = 0;
 var growing = true;
@@ -14,7 +17,6 @@ var animateCircle = function(){
     x = 0;
     growing = true; 
     drawCircle();
-    
     
 }
 
@@ -30,6 +32,7 @@ var drawCircle = function(){
 	x++;
     else
 	x--;
+    //if touching edges switch to shrinking
     if(x === 250)
 	growing = false;
     if(x === 0)
@@ -43,8 +46,59 @@ var freeze = function(e){
 }
 
 
-
-
 animate.addEventListener('click',animateCircle);
 start.addEventListener('click',drawCircle);
 stop.addEventListener('click',freeze);
+
+
+//Bouncing
+
+//Initializing Variables
+//dimensions of rectangle
+var length = 100;
+var width = 50;
+//top left corner
+var xcor = 250;
+var ycor = 250;
+//bottom right corner 
+var xcorend = xcor + length;
+var ycorend = ycor + width;
+//incrementing for movement
+var xinc = 3;
+var yinc = 2;
+
+
+var bouncing = function(){
+    window.cancelAnimationFrame(requestID);
+
+    xcor = Math.floor(Math.random() * (500-length));
+    ycor = Math.floor(Math.random() * (500-width));
+    xcorend = xcor + length;
+    ycorend = ycor + width;
+    
+    drawRect();
+}
+
+
+var drawRect = function(){
+    window.cancelAnimationFrame(requestID);
+    ctx.clearRect(0, 0, 500, 500);
+    ctx.beginPath();
+    ctx.fillStyle = "blue";
+    ctx.fillRect(xcor,ycor,length,width);
+    ctx.stroke();
+    //if rectangle is touching edges
+    // change direction
+    if(xcor <=0 || xcorend >= 500)
+	xinc *= -1;
+    if(ycor <=0 || ycorend >= 500)
+	yinc *= -1;
+    xcor += xinc;
+    ycor += yinc;
+    xcorend = xcor + length;
+    ycorend = ycor + width;
+    requestID = window.requestAnimationFrame(drawRect);
+}
+    
+bounce.addEventListener('click',bouncing);
+cont.addEventListener('click',drawRect);
